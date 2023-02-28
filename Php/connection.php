74598adc2d412a7991106f704a283c201830f7d0
TediSalivio26 <TediSalivio@gmail.com>
2023-02-28 17:49:27 +0800

    Final edit

--- Php/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'dino_database');
-$db = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
